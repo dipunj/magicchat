cb34825e54d00014ddbaa5fdb5ee5af29c1e387a
Dipunj Gupta <dipunj.mnnit@gmail.com>
2018-03-17 13:31:05 +0530

    functional bot

--- telegram-bot/index.py
+++ telegram-bot/index.py
@@ -20,6 +20,8 @@ bot.
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
 
+chats = []
+
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
@@ -30,19 +32,22 @@ logger = logging.getLogger(__name__)
 # update. Error handlers also receive the raised TelegramError object in error.
 def start(bot, update):
     """Send a message when the command /start is issued"""
-    bot.send_message(chat_id=458059323, text="Kesarwani")
+    # bot.send_message(chat_id=458059323, text="Kesarwani")
     update.message.reply_text('Hi!')
 
 
 def help(bot, update):
     """Send a message when the command /help is issued."""
-    update.message.reply_text('Help!')
+    update.message.reply_text()
 
+def driver(bot, update):
+    """Record the user messages."""
+    chats.append(update.message.text)
 
-def echo(bot, update):
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
-
+def juice(bot, update):
+    """Prints all the messages."""
+    update.message.reply_text("THE JUICE IS : " + str(chats))
+    chats = []
 
 def error(bot, update, error):
     """Log Errors caused by Updates."""
@@ -60,9 +65,10 @@ def main():
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("juice", juice))
 
-    # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, echo))
+    # on noncommand i.e message - driver the message on Telegram
+    dp.add_handler(MessageHandler(Filters.text, driver))
 
     # log all errors
     dp.add_error_handler(error)
